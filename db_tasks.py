a7c0cb12ebd2ef0886ad2289c1f03f67c509a62b
Nat Gaertner <natgaertner@gmail.com>
2014-03-22 11:12:03 +0000

    new db fields

--- db_tasks.py
+++ db_tasks.py
@@ -2,6 +2,6 @@ from postgres_conn import get_connection
 
 def insert_mash(data):
     conn = get_connection('postmash')
-    sql = "INSERT INTO mashes(winnerid, loserid, timestamp,remote_addr) VALUES (%s, %s, %s, %s)"
-    conn.cursor().execute(sql, (data['winner'], data['loser'], data['timestamp'], data['remote_addr']))
+    sql = "INSERT INTO mashes(leftid, rightid,winner,timestamp,remote_addr) VALUES (%s, %s, %s, %s)"
+    conn.cursor().execute(sql, (data['leftid'], data['rightid'], data['winner'], data['timestamp'], data['remote_addr']))
     conn.commit()
